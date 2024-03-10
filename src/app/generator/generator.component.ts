95ce9c84df0c8081165fde6ff82518213b64606a
iamlydial <lydia.lavecchia@gmail.com>
2024-03-10 17:44:03 +0100

    Background color function
    added bg color function and added some headings styling to the frontend

--- src/app/generator/generator.component.ts
+++ src/app/generator/generator.component.ts
@@ -1,4 +1,5 @@
 import { Component, ElementRef, ViewChild } from '@angular/core';
+import { ColorEvent } from 'ngx-color';
 import { reduce } from 'rxjs';
 
 @Component({
@@ -11,6 +12,8 @@ export class GeneratorComponent {
 topText : string = '';
 bottomText : string = '';
 fileEvent: any;
+textColor: string = '#000000';
+backgroundColor: string = '#f9f9f9';
 
 preview(e: any) {
   this.fileEvent = e;
@@ -49,16 +52,35 @@ drawText(){
   let ctx = canvas.getContext('2d');
   
   ctx?.clearRect(0,0,canvas.width, canvas.height);
+
   this.preview(this.fileEvent);
 
+  // Check if this.fileEvent is defined before calling preview
+  if (this.fileEvent) {
+    this.preview(this.fileEvent);
+  }
+
   if (ctx) {
-    ctx.fillStyle = '#000000';
+    ctx.fillStyle = this.backgroundColor;
+    ctx?.fillRect(0,0,canvas.width, canvas.height);
+    
+    ctx.fillStyle = this.textColor;
     ctx.font = '50px Comic Sans MS';
     ctx.textAlign = 'center'
     ctx.fillText(this.topText, canvas.width / 2, 100);
     ctx.fillText(this.bottomText, canvas.width / 2, 750);
   }
   
+}
+
+canvasTextColor($event: ColorEvent){
+  this.textColor = $event.color.hex;
+  this.drawText();
+}
+
+canvasBgColor($event: ColorEvent){
+  this.backgroundColor = $event.color.hex;
+  this.drawText();
 }
 
 }
