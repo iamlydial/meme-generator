fe9ff5dc85a96410842d896130c0491f296cb28f
iamlydial <lydia.lavecchia@gmail.com>
2024-03-10 17:14:50 +0100

    Added Drawing Text function
    for both top and bottom text, created a global event variable to pass the event from the preview image to the the draw text function.
    edited the height of the image to be just 70% of the canvas height so that the text is visible

--- src/app/generator/generator.component.ts
+++ src/app/generator/generator.component.ts
@@ -8,9 +8,12 @@ import { reduce } from 'rxjs';
 })
 export class GeneratorComponent {
 @ViewChild('memeCanvas', { static: false }) myCanvas!: ElementRef<HTMLCanvasElement>;
-
+topText : string = '';
+bottomText : string = '';
+fileEvent: any;
 
 preview(e: any) {
+  this.fileEvent = e;
   if (this.myCanvas && this.myCanvas.nativeElement) {
     let canvas = this.myCanvas.nativeElement;
     let ctx = canvas.getContext('2d');
@@ -18,27 +21,17 @@ preview(e: any) {
     let render = new FileReader();
     render.readAsDataURL(e.target.files[0]);
 
-    render.onload = function(event) {
+    render.onload = function (event) {
       const img = new Image();
       img.src = event.target?.result as string;
-      img.onload = function() {
+      img.onload = function () {
         // Calculate the aspect ratio of the image
         const aspectRatio = img.width / img.height;
 
-        // Calculate the dimensions based on the aspect ratio
-        let width = img.width;
-        let height = img.height;
-
-        // Adjust dimensions to fit within the canvas while maintaining the aspect ratio
-        if (width > canvas.width) {
-          width = canvas.width;
-          height = width / aspectRatio;
-        }
-
-        if (height > canvas.height) {
-          height = canvas.height;
-          width = height * aspectRatio;
-        }
+        // Calculate the dimensions based on the aspect ratio and desired height
+        const desiredHeight = (canvas.height * 0.7); // 70% of the canvas height
+        const width = desiredHeight * aspectRatio;
+        const height = desiredHeight;
 
         // Center the image on the canvas
         const x = (canvas.width - width) / 2;
@@ -49,6 +42,23 @@ preview(e: any) {
       };
     };
   }
+}
+
+drawText(){
+  let canvas = this.myCanvas.nativeElement;
+  let ctx = canvas.getContext('2d');
+  
+  ctx?.clearRect(0,0,canvas.width, canvas.height);
+  this.preview(this.fileEvent);
+
+  if (ctx) {
+    ctx.fillStyle = '#000000';
+    ctx.font = '50px Comic Sans MS';
+    ctx.textAlign = 'center'
+    ctx.fillText(this.topText, canvas.width / 2, 100);
+    ctx.fillText(this.bottomText, canvas.width / 2, 750);
+  }
+  
 }
 
 }
