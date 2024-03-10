dc6cf60f545ffa8b5e596fa912302e051b3cae72
iamlydial <lydia.lavecchia@gmail.com>
2024-03-10 14:00:07 +0100

    Nabvar
    installed bootstrap and cerated the navbar component which has been styled and utilizes google fonts.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,14 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NavbarComponent } from './navbar/navbar.component';
+import { GeneratorComponent } from './generator/generator.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    GeneratorComponent
   ],
   imports: [
     BrowserModule,
